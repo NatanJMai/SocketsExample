4f7690a4ed50e5c45f34fb4296de6f8ed1957f6a
NatanJMai <natan.mai@hotmail.com>
2016-05-31 00:17:32 -0300

    Add client and server files.

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+
+def connect(HOST, PORT, FILE):
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   s.connect((HOST, PORT))
+
+   f = open(FILE, 'r')
+
+   for i in f:
+      s.send(i.encode())
+      data = s.recv(1024)
+      print("rcv ", repr(data))
+   f.close()
+   s.close()
+
+
+def main():
+   connect('10.1.1.29', 50008, "file.txt")
+
+if __name__ == '__main__':
+   main()
