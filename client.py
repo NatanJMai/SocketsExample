268e86c7039e0e47cb9d6f2bf009f5e34f4aefff
NatanJMai <natan.mai@hotmail.com>
2016-05-31 22:08:18 -0300

    Update client/server files

--- client.py
+++ client.py
@@ -2,16 +2,20 @@ import socket
 
 def connect(HOST, PORT, FILE):
    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   s.connect((HOST, PORT))
-
-   f = open(FILE, 'r')
-
-   for i in f:
-      s.send(i.encode())
-      data = s.recv(1024)
-      print("rcv ", repr(data))
-   f.close()
-   s.close()
+	
+   try:
+      s.connect((HOST, PORT))
+   except:
+      print("Connection refused.")
+      return
+   
+   with open(FILE, 'r') as f:
+      for i in f:
+         s.send(i.encode())
+         data = s.recv(1024)
+         print("rcv ", repr(data))
+      f.close()
+      s.close()
 
 
 def main():
--- server.py
+++ server.py
@@ -3,15 +3,23 @@ import socket
 HOST = ''
 PORT = 50008
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-   s.bind((HOST, PORT))
-   s.listen(1)
-   conn, addr = s.accept()
+def read(line):
+	pass
 
-   with conn:
-      print('Connected by ', addr)
-      while True:
-         data = conn.recv(1024)
-         print(data.decode())
-         if not data:break
-         conn.sendall(data)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.bind((HOST, PORT))
+
+	while True:
+		s.listen(1)
+		conn, addr = s.accept()
+		
+		with conn:
+			print('Connected by ', addr)
+			while True:
+				rcve = (conn.recv(1024)).decode() #Receive and decode info
+				print(rcve)
+				
+				read(rcve)
+				
+				if not rcve: break
+				conn.sendall(rcve.encode())
