bfe3160b1234c8ad80d4e8da49b54b791713bb3f
NatanJMai <natan.mai@hotmail.com>
2016-06-02 23:18:45 -0300

    Almost done

--- server.py
+++ server.py
@@ -1,25 +1,47 @@
 import socket
 
-HOST = ''
-PORT = 50008
+def abort(msg):
+   print(msg)
+   exit()
 
 def read(line):
-	pass
+   tot = 0
+   spl = line.split()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.bind((HOST, PORT))
+   if spl != []: tot = float(spl[1]) * float(spl[2])
+   return tot
 
-	while True:
-		s.listen(1)
-		conn, addr = s.accept()
-		
-		with conn:
-			print('Connected by ', addr)
-			while True:
-				rcve = (conn.recv(1024)).decode() #Receive and decode info
-				print(rcve)
-				
-				read(rcve)
-				
-				if not rcve: break
-				conn.sendall(rcve.encode())
+def connect(HOST, PORT):
+   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.bind((HOST, PORT))
+
+      while True:
+         s.listen(1)
+         
+         try:
+            conn, addr = s.accept()
+         except:
+            abort("\nClosing.")
+            return
+
+         with conn:
+            print('Connected by ', addr)
+
+            tot = 0.0
+
+            while True:
+               rcve = (conn.recv(1024)).decode() #Receive and decode info
+               
+               if not rcve: break
+
+               tot += read(rcve)
+               conn.sendall(rcve.encode())
+            print("Final Result-> ", tot)
+
+
+if __name__ == '__main__':
+   HOST = ''
+   PORT = 50008
+
+   # Main function
+   connect(HOST, PORT)
